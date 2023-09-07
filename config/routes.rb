bc751f765d098c71c2032805193385e23f0f4f30
valrite ehachi <valritehachi@gmail.com>
2023-09-07 18:17:37 +0300

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'hesabu#index'
-  get '/', to: 'hesabu#index'
+  get '/index', to: 'hesabu#index'
   get '/login', to: 'hesabu#login'
   get '/instructions', to: 'hesabu#instructions'
   get '/start', to: 'hesabu#start'
