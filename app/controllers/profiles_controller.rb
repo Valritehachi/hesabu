78c59510524d17addf847272cf93f0d70166c821
valrite ehachi <valritehachi@gmail.com>
2023-09-10 23:31:25 +0300

    updates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,36 @@
 class ProfilesController < ApplicationController
 
     def create
+        user_fields = params[:user]
+        unless user_fields.present?
+            flash[:error] = "Something went wrong. Please try again."
+            redirect_to :back
+        end
+
+        Rails.logger.debug "User Fields #{user_fields}"
+        user = User.where(email: user_fields[:email].downcase).first
+        unless user.present?
+            flash[:error] = "user already exists."
+            redirect_to :back
+        end
+
+        user = User.create(user_fields)
+        unless user.valid?
+            flash[:error] = "Error creating user: #{user.errors.to_s}."
+            redirect_to :back
+        end
+
+        player_data = params[:players]
+        if player_data.present?
+            player_data.each do |player_fields|
+                player = user.players.create(
+                    player_fields 
+                )
+            end
+        end
+
+        flash[:notice] = "Created user: #{user.email} and #{user.players.count} Players"
+        redirect_to '/instructions'
     end
 
     def update
