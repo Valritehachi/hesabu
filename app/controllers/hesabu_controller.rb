1d6281cc6340d28e4975cc8fcfc6d9a1c0b3920d
valrite ehachi <valritehachi@gmail.com>
2023-09-13 07:41:10 +0300

    updates

--- app/controllers/hesabu_controller.rb
+++ app/controllers/hesabu_controller.rb
@@ -1,26 +1,59 @@
 class HesabuController < ApplicationController
 
     layout "hesabu"
+    def initialize
+        super
+        @current_page = 'home'
+        @pages = {
+            "home" => {
+                "title" => "Home",
+                "path" => "/"
+            },
+            "gameinstructions" => {
+                "title" => "Instructions",
+                "path" => "/gameinstructions"
+            },
+            "login" => {
+                "title" => "Login",
+                "path" => "/login"
+            },
+            "signup" => {
+                "title" => "Sign Up",
+                "path" => "/signup"
+            },
+            "profile" => {
+                "title" => "Profile",
+                "path" => "/profile"
+            }
+        }
+    end
+
+
     
     def index
     end
 
     def login
+        @current_page = 'login'
     end
 
     def instructions 
+        @current_page = 'instructions'
     end
     
     def start
+        @current_page = 'start'
     end
 
     def signup
+        @current_page = 'signup'
     end
 
     def settings
     end
 
     def gameinstructions
+        @current_page = 'gameinstructions'
     end
 
     def feedback
