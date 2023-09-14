aae507c1142b3a279361d5fff17be94943b10885
valrite ehachi <valritehachi@gmail.com>
2023-09-14 20:46:38 +0300

    edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
   has_many :players, dependent: :destroy
-  def  name
+  def name(email)
     email.split('@').first
   end
   
