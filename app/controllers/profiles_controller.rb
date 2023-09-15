e2f5dc6b8738950d778735e1fd845391e1cadd8a
valrite ehachi <valritehachi@gmail.com>
2023-09-15 08:08:02 +0300

    updates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -36,6 +36,46 @@ class ProfilesController < ApplicationController
     end
 
     def update
+        @user = User.find(params[:id])
+        user_fields = params.require(:user).permit!
+
+        attributes = {}
+        if user_fields['email'] != @user.email
+            attributes['email'] = user_fields['email']
+        end
+        if !user_fields['password'].nil?
+            attributes['password'] = user_fields['password']
+        end
+        if attributes.size > 0
+            @user.update(attributes)
+        end
+
+        player_data = params["players"] #require(:player"s).permit!
+        if player_data.present?
+            player_data.each do |data|
+                player_fields = data.permit!
+                player_id   = player_fields['id']
+                player_name = player_fields['name']
+                player_age  = player_fields['age']
+                Rails.logger.debug("PLAYER: #{player_fields}")
+                if player_id
+                    player = @user.players.where(id: player_id).first
+                    if player_name&.length > 0 
+                        player.update(
+                            name: player_name,
+                            age: player_age.to_i
+                        )
+                    end
+                else 
+                    player = user.players.create(
+                        name: player_name,
+                        age: player_age.to_i
+                    )
+                end
+            end
+        end
+        flash[:notice] = "Updated Profile Successfully"
+        redirect_to '/instructions'
     end
 
     def destroy
