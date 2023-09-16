dc3fc8ca0eb5e395a9ff12a38bfa9a74340303c7
valrite ehachi <valritehachi@gmail.com>
2023-09-16 08:26:54 +0300

    updates

--- app/assets/javascript/hesabu.js
+++ app/assets/javascript/hesabu.js
@@ -1,105 +1,55 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Your code here
-  var element = document.getElementById('get_started');
-  console.log(element)
-  // element.addEventListener("click", function() {
-  //       alert("Button clicked!");
-  //     });
-  var loginButton = document.getElementById('login');
-  var signUpButton = document.getElementById('sign-up');
-  console.log(loginButton,signUpButton)
-  //loginButton.addEventListener("click", function() {
-  // alert("Login button clicked!");
-  //});
+(function (Hesabu,window,undefined){
 
- // signUpButton.addEventListener("click", function() {
- //    alert("Sign Up button clicked!");
-
-  var additionButton = document.getElementById('addition');
-  var subtractionButton = document.getElementById('subtraction');
-  var multiplicationButton = document.getElementById('multiplication');
-  var divisionButton = document.getElementById('division');
-
-  console.log(additionButton, subtractionButton, multiplicationButton, divisionButton);
-
-  //additionButton.addEventListener("click", function() {
-  //alert("addition button clicked!");
-  //});
-
-  subtractionButton.addEventListener("click", function() {
-    alert("subtraction button clicked!");
-  });
-
-  multiplicationButton.addEventListener("click", function() {
-    alert("multiplication button clicked!");
-  });
-
-  divisionButton.addEventListener("click", function() {
-    alert("division button clicked!");
-  });
+  Hesabu = function() {
+		this.config = {
+      boundEvents: false
+    };
+  };
 
 
-  var easybutton = document.getElementById('easy');
-  var mediumbutton = document.getElementById('medium');
-  var hardbutton = document.getElementById('hard');
-  var startbutton = document.getElementById('start');
-  console.log(easybutton,mediumbutton ,hardbutton ,startbutton )
-  easybutton.addEventListener("click", function() {
-    alert("easy button clicked!");
-  });
+  Hesabu.prototype.InitInterface = function()
+  {
+    console.log('Hesabu Initializing');
+    if(this.config['boundEvents'] == false){
+      window.Hesabu.bindEvents();
+    }
+  };
 
-  mediumbutton.addEventListener("click", function() {
-    alert("medium button clicked!");
-  });
-  hardbutton.addEventListener("click", function() {
-    alert("hard button clicked!");
-  });
-  startbutton.addEventListener("click", function() {
-    alert("start button clicked!");
-  });
+  Hesabu.prototype.bindEvents = function()
+  {
+    // $('.quote-wrapper').off('change','.pricing-customer-type');
+    // $('.quote-wrapper').on(
+    //   'change',
+    //   '.pricing-customer-type',
+    //    window.Hesabu.onChangePricingCustomerType
+    // );
 
-  var element =document.getElementById('Sign_Up');
-  console.log(element)
-  element.addEventListener("click", function() {
-      alert("Button clicked!");
-  });
+    $('body').off('click','a.logout-link');
+    $('body').on(
+      'change',
+      'a.logout-link',
+       window.Hesabu.onClickLogout
+    );
 
-    var saveprogressButton = document.getElementById('save progress');
-    var resetprogressButton = document.getElementById('reset-progress');
-    console.log(saveprogressButton,resetprogressButton)
-    resetprogressButton.addEventListener('click', function() {
-      // Display a confirmation prompt
-      const confirmReset = confirm("Are you sure you want to reset the game progress?");
-    
-      // Check the user's response
-      if (confirmReset) {
-        // If the user confirms, reset the game progress (you can add your reset logic here)
-        alert("Game progress has been reset.");
-        // Add your reset logic here, such as resetting scores or game state
-      } else {
-        // If the user cancels, do nothing
-        alert("Game progress was not reset.");
-      }
-    });
+  };
 
+  Hesabu.prototype.onClickLogout = function(e)
+  {
+    e.preventDefault();
+    console.log("logout link clicked");
+  };
 
-});
+  window.Hesabu = new Hesabu();
 
-$(document).ready(function () {
-  var maxPlayers = 5;
+  window.addEventListener("load", function load(event){
+    window.removeEventListener("load", load, false); //remove listener, no longer needed
+    console.log("Hesabu MODULE LOADED");
+    window.Hesabu.InitInterface();
+  },false);
 
-  $("#add-player-button").click(function () {
-      var currentPlayers = $("#player-list .player").length;
+})(window.Hesabu=window.Hesabu||{},window);
 
-      if (currentPlayers < maxPlayers) {
-          // Create a new player element
-          var newPlayer = $("<div class='player'>Player " + (currentPlayers + 1) + "</div>");
-          $("#player-list").append(newPlayer);
-      } else {
-          alert("You've reached the maximum number of players (5)!");
-      }
-  });
-});
+  
 
 
 
