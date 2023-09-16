056721327232ecf693627b2b861a8b3acad9c780
valrite ehachi <valritehachi@gmail.com>
2023-09-16 08:34:29 +0300

    updates

--- app/assets/javascript/hesabu.js
+++ app/assets/javascript/hesabu.js
@@ -26,7 +26,7 @@
 
     $('body').off('click','a.logout-link');
     $('body').on(
-      'change',
+      'click',
       'a.logout-link',
        window.Hesabu.onClickLogout
     );
@@ -37,6 +37,34 @@
   {
     e.preventDefault();
     console.log("logout link clicked");
+    window.Hesabu.ForceLogout();
+  };
+
+  Hesabu.prototype.ForceLogout = function()
+    {
+      var AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
+      console.log("Logging out....see ya!");
+
+      $.ajax({
+         url: '/users/sign_out',
+         data: {
+              authenticity_token: AUTH_TOKEN
+         },
+         context: $(document),
+         cache: false,
+         type: 'DELETE',
+         dataType: 'json',
+         error: function (jqXHR,str)
+         {
+           console.log("ERROR: ", str);
+         },
+         success: function(data,textStatus,jqXHR)
+         {
+            window.location.href = window.location.href;
+         }
+       });
+    };
+
   };
 
   window.Hesabu = new Hesabu();
