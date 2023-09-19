16f9d0452e2b713ed930f97a8fab64946f495fcd
valrite ehachi <valritehachi@gmail.com>
2023-09-19 08:02:37 +0300

    updates

--- app/assets/javascript/games/hello.js
+++ app/assets/javascript/games/hello.js
@@ -1,5 +1,37 @@
 (function (HelloGame,window,undefined){
 
+    class Example extends Phaser.Scene
+    {
+        preload ()
+        {
+            this.load.setBaseURL('http://labs.phaser.io');
+            this.load.image('sky', 'assets/skies/space3.png');
+            this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+            this.load.image('red', 'assets/particles/red.png');
+        }
+
+        create ()
+        {
+            this.add.image(400, 300, 'sky');
+
+            var particles = this.add.particles('red');
+
+            var emitter = particles.createEmitter({
+                speed: 100,
+                scale: { start: 1, end: 0 },
+                blendMode: 'ADD'
+            });
+
+            var logo = this.physics.add.image(400, 100, 'logo');
+
+            logo.setVelocity(100, 200);
+            logo.setBounce(1, 1);
+            logo.setCollideWorldBounds(true);
+
+            emitter.startFollow(logo);
+        }
+    }
+
     HelloGame = function() {
           this.config = {
             type: Phaser.CANVAS,
@@ -12,10 +44,7 @@
                     gravity: { y: 200 }
                 }
             },
-            scene: {
-                preload: window.HelloGame.preload,
-                create: window.HelloGame.create
-            }
+            scene: Example
         };
     };
   
