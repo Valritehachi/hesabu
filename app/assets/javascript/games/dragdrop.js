430f11cba4339d9f9990153c3c4d6bbdd67cbfc0
valrite ehachi <valritehachi@gmail.com>
2023-09-19 18:52:10 +0300

    updates

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -3,32 +3,31 @@
         preload() {
             this.load.setBaseURL('https://labs.phaser.io');
             //this.load.image('sky', 'assets/skies/space3.png');
-            this.load.image('number4', 'assets/images/hesabu/number4.png');
             this.load.image('red', 'assets/particles/red.png');
         }
 
         create() {
-            // Create a number4 sprite with physics
-            const number4 = this.physics.add.image(400, 300, 'number4');
-
-            // Set initial velocity for bouncing
-            number4.setVelocity(200, 200);
-            number4.setBounce(1, 1);
-            number4.setCollideWorldBounds(true);
-
-            // Create particles for visual effect
-            const particles = this.add.particles('red');
-            const emitter = particles.createEmitter({
-                speed: 100,
-                scale: { start: 1, end: 0 },
-                blendMode: 'ADD'
-            });
-
-            // Start emitting particles and follow the number4 sprite
-            emitter.startFollow(number4);
-
             // Create a background image
             this.add.image(400, 300, 'sky');
+
+            // Create a physics group for the numbers (text objects)
+            const numbersGroup = this.physics.add.group();
+
+            // Create and add numbers to the group
+            for (let i = 0; i < 10; i++) {
+                const numberText = this.add.text(Phaser.Math.Between(50, 750), Phaser.Math.Between(50, 550), i.toString(), { fontSize: '32px', fill: '#fff' });
+
+                // Enable physics for text objects
+                this.physics.world.enable(numberText);
+
+                // Set physics properties for bouncing
+                numberText.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+                numberText.setBounce(1, 1);
+                numberText.setCollideWorldBounds(true);
+
+                // Add text objects to the group
+                numbersGroup.add(numberText);
+            }
         }
     }
 
@@ -55,6 +54,12 @@
         window.HelloGame.config.canvas = canvas;
         var game = new Phaser.Game(window.HelloGame.config);
     };
+
+    HelloGame.prototype.preload = function () {
+        this.load.setBaseURL('https://labs.phaser.io');
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('red', 'assets/particles/red.png');
+    }
 
     window.HelloGame = new HelloGame();
 
