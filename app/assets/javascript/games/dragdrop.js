690d26985b48a3645a6b7721d3431447e0f5406f
valrite ehachi <valritehachi@gmail.com>
2023-09-21 14:15:17 +0300

    updates

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -29,7 +29,8 @@ class Example extends Phaser.Scene {
         //this.ground.body.immovable = true;
          this.add.text(16, 16, 'Drag the Sprite').setFontSize(24).setShadow(1, 1);
      
-         this.sprite = this.physics.add.sprite(180, 240, 'digit_1' ,);
+         this.sprite = this.physics.add.sprite(180, 240, 'digit_0',);
+         this.sprite = this.physics.add.sprite(180, 240, 'digit_1',);
          //this.sprite.body.setGravity(300);
          this.sprite.setBounce(0.2);
             this.sprite.setCollideWorldBounds(true);
