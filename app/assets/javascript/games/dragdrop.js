0a56931637fead77503bc437c3952bacb7474d5d
valrite ehachi <valritehachi@gmail.com>
2023-09-25 18:21:44 +0300

    updates

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -28,7 +28,7 @@ class Example extends Phaser.Scene {
         this.add.text(16, 16, 'Drag the Sprite').setFontSize(24).setShadow(1, 1);
        
         this.platforms = this.physics.add.staticGroup();
-        const rect = this.add.rectangle(100, 310, 150, 20, 0xff66ff);
+        const rect = this.add.rectangle(100, 310, 130, 20, 0xff66ff);
         this.platforms.add(rect);
         //x       =====  
         //       =   =
@@ -57,7 +57,7 @@ class Example extends Phaser.Scene {
         r1.setStrokeStyle(4, 0xbcffff);
         this.platforms.add(r1);
         
-        const rect1 = this.add.rectangle(400, 310, 150, 20, 0xff66ff);
+        const rect1 = this.add.rectangle(400, 310, 130, 20, 0xff66ff);
         this.platforms.add(rect1);
 
         // the equal sign 
