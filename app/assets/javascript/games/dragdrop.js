5f62a6ff99dbaa110c15a047899e3fd31a0427fb
valrite ehachi <valritehachi@gmail.com>
2023-09-26 06:55:28 +0300

    edits

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -67,7 +67,7 @@ class Example extends Phaser.Scene {
         const rect3 = this.add.rectangle(550, 325, 75, 15, 0x6666ff);
         rect3.setStrokeStyle(2, 0xefc53f); 
         
-        const rect4 = this.add.rectangle(700, 310, 180, 20, 0xff66ff);
+        const rect4 = this.add.rectangle(725, 310, 180, 20, 0xff66ff);
         this.platforms.add(rect4);
         //Create the bucket
         //this.bucket = this.physics.add.image(900, 400, 'bucket');
