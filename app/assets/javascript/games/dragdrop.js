4ddcd549b9c5bcbc4d5284c96a723c58b5c65d78
valrite ehachi <valritehachi@gmail.com>
2023-10-03 07:22:18 +0300

    edits

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -109,7 +109,7 @@ class Example extends Phaser.Scene {
         this.math_problem['sum'] = randomSum;
 
         // Create a static text object
-        const staticText = this.add.text(100, 500, 
+        const staticText = this.add.text(0, 500, 
             'pick any two numbers that add upto the number after the equal sign', {
             fontFamily: 'Arial Black',
             fontSize: '30px',
