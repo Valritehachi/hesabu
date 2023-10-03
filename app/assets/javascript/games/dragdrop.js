d0aa641783e687b27a2f599111d49660279ffc55
valrite ehachi <valritehachi@gmail.com>
2023-10-03 07:02:11 +0300

    edits

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -87,6 +87,18 @@ class Example extends Phaser.Scene {
         
         const rect4 = this.add.rectangle(725, 430, 200, 20, 0xff66ff);
         this.platforms.add(rect4);
+        // Create a static text object
+        const staticText = this.add.text(100, 100, 'pick any two numbers that add upto the number after the equal sign', {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            color: '#000000'
+        });
+
+     // Enable Arcade Physics for the text object
+        this.physics.world.enable(staticText);
+
+        // Make the text static (immovable)
+        staticText.body.moves = false;
 
         const randomSum = this.getRandomSum();
         const tens = Math.floor(randomSum / 10);
@@ -181,18 +193,7 @@ class Example extends Phaser.Scene {
     }
     
     create() {
-        // Create a static text object
-        const staticText = this.add.text(100, 100, 'pick any two numbers that add upto the number after the equal sign', {
-            fontFamily: 'Arial',
-            fontSize: '24px',
-            color: '#ffffff'
-        });
-    
-        // Enable Arcade Physics for the text object
-        this.physics.world.enable(staticText);
-    
-        // Make the text static (immovable)
-        staticText.body.moves = false;
+       
     }
     update(){
         
