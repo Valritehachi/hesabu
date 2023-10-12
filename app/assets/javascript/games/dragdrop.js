77db0ece5430fd38b9d716e151adf25378fbf342
valrite ehachi <valritehachi@gmail.com>
2023-10-12 05:15:09 +0300

    edits

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -184,9 +184,9 @@ class Example extends Phaser.Scene {
                 
             
                 const tensDigit = this.math_problem['addend1_digit'];
-                tensDigit.setGravityY(0);
+                tensDigit.setGravityY(-350);
                 const onesDigit = this.math_problem['addend2_digit'];
-                onesDigit.setGravityY(0);
+                onesDigit.setGravityY(-350);
                 this.math_problem['addend1'] = null; 
                 this.math_problem['addend2'] = null;
                 const upwardVelocity = -350;
