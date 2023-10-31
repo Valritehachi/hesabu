f0b50931ef39101c018aed8a7e06281c3e0af79e
valrite ehachi <valritehachi@gmail.com>
2023-10-31 07:27:24 +0300

    edits

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -1,7 +1,7 @@
 class Button {
 
     button = null;
-    
+
     constructor(x, y, label, scene, callback) {
         this.button = scene.add.text(x, y, label)
             .setOrigin(0.5)
@@ -9,8 +9,8 @@ class Button {
             .setStyle({ backgroundColor: '#111' })
             .setInteractive({ useHandCursor: true })
             .on('pointerdown', () => callback())
-            .on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
-            .on('pointerout', () => button.setStyle({ fill: '#FFF' }));
+            .on('pointerover', () => this.button.setStyle({ fill: '#f39c12' }))
+            .on('pointerout', () => this.button.setStyle({ fill: '#FFF' }));
     }
     destroy() {
         this.button.destroy();
