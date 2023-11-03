762f4c0b70124dbc25e173c279ab796303e0e6d2
valrite ehachi <valritehachi@gmail.com>
2023-11-03 18:13:35 +0300

    aadjusted bounce

--- app/assets/javascript/games/dragdrop.js
+++ app/assets/javascript/games/dragdrop.js
@@ -251,7 +251,7 @@ class Example extends Phaser.Scene {
             window.DragDropGame.config.height/2,
             'game_over');
         
-        this.gameOver.setBounce(1);
+        this.gameOver.setBounce(0.5);
         this.gameOver.setCollideWorldBounds(true);
         this.gameOver.setVisible(false);
         this.gameOver.setDepth(9999);
