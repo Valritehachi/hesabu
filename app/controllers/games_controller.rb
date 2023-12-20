0b8bc37a6c115e800695ca2a5aca1a5222540a43
valrite ehachi <valritehachi@gmail.com>
2023-12-20 06:44:34 +0300

    added levels

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,7 +9,14 @@ class GamesController < ApplicationController
     end
 
     def mads
+        difficulties = {
+            'add': %w[AAB ABA BBA],
+            'sub': %w[AAB ABA BBA],
+            'mul': %w[AAB ABA BBA],
+            'div': %w[AAB ABA BBA]
+        }
         @type = params[:type] || 'add'
+        @difficulty = difficulties[@type]
     end
 
 end
